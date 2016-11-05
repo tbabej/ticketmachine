aee70068239ca291bae3ef0ee7228205251f65b6
Tomas Babej <tomasbabej@gmail.com>
2016-11-06 00:08:58 +0100

    main: Fix plugin loading

--- ticketmachine/main.py
+++ ticketmachine/main.py
@@ -78,17 +78,17 @@ class TicketMachine(LoggerMixin):
             self.error('Machine "{0}" is not available'.format(identifier))
 
     def import_plugins(self):
-        import machines
+        import ticketmachine.machines
 
         # pylint: disable=broad-except
-        for module in machines.__all__:
+        for module in ticketmachine.machines.__all__:
             try:
-                module_id = "{0}.{1}".format('machines', module)
+                module_id = "{0}.{1}".format('ticketmachine.machines', module)
                 importlib.import_module(module_id)
                 self.debug(module_id + " loaded successfully.")
             except Exception as exc:
                 self.warning(
-                    "The {0} module could not be loaded: {1} "
+                    "The ticketmachine.{0} module could not be loaded: {1} "
                     .format(module, str(exc)))
                 self.log_exception()
 
